c21e79c7ef3fa3ca1bfdcf0f81fde0f0d9052aa3
Pavel Maltsev <maltsevpa@live.ru>
2023-01-27 12:44:09 +0300

    Добавьте файлы проекта.

---
+++ TreeDataGridDemo/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TreeDataGridDemo.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
